3614c7bc42ba1ad774cd12f3312547ae3764caef
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2019-02-04 09:41:19 +0800

    add JSErrorForm validation in create view

--- backend/js_error_logger/views.py
+++ backend/js_error_logger/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
 from .forms import JSErrorForm
 
 
 def create(request):
     f = JSErrorForm(request.POST)
-    f.save()
+    if f.is_valid():
+        f.save()
+        return HttpResponse(status=201)
+    else:
+        return JsonResponse(f.errors, status=400)
